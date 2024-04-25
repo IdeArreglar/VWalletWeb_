5def97dbac7f358d123dba9f538e7d6b0a084a0d
arianjsx <u202116567@upc.edu.pe>
2024-04-25 13:26:04 -0500

    Update TipoUsuarioController
    Implemented lombok

--- src/main/java/pe/edu/upc/vwalletweb/controllers/TipoUsuarioController.java
+++ src/main/java/pe/edu/upc/vwalletweb/controllers/TipoUsuarioController.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.vwalletweb.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import pe.edu.upc.vwalletweb.serviceinterfaces.ITipoUsuarioService;
 
 @RestController
 @RequestMapping("/tipousuario")
+@RequiredArgsConstructor
 public class TipoUsuarioController {
-    @Autowired
     private ITipoUsuarioService tS;
 }
