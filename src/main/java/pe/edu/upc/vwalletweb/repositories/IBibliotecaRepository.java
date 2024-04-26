9b1ab9fb0780d9ee78ea066703f365201162af03
Csar Mart1ne7 <cxsxr17@gmail.com>
2024-04-26 17:33:54 -0500

    Query libros
    corrigiendo datos

--- src/main/java/pe/edu/upc/vwalletweb/repositories/IBibliotecaRepository.java
+++ src/main/java/pe/edu/upc/vwalletweb/repositories/IBibliotecaRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @Repository
 public interface IBibliotecaRepository extends JpaRepository<Biblioteca, Integer> {
+
+    // Query 6
     @Query(value = "SELECT b.sede_biblioteca, COUNT(l.id_libro) AS cantidad_libros\n" +
             "FROM biblioteca b\n" +
             "JOIN libro l ON b.id_biblioteca = l.biblioteca_id\n" +
