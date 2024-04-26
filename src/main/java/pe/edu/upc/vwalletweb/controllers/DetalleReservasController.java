9715ddce50831a90b7129d2b35f48a0324812771
Jair Leon <jair.mathias07@gmail.com>
2024-04-26 17:14:28 -0500

    implementación query 5 y 7
    nativequery

--- src/main/java/pe/edu/upc/vwalletweb/controllers/DetalleReservasController.java
+++ src/main/java/pe/edu/upc/vwalletweb/controllers/DetalleReservasController.java
@@ -4,9 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.vwalletweb.dtos.DetalleReservasDTO;
+import pe.edu.upc.vwalletweb.dtos.TotalGastoxUsuarioDTO;
+import pe.edu.upc.vwalletweb.dtos.totalViajesRealizadosPorTransporteidDTO;
 import pe.edu.upc.vwalletweb.entities.DetalleReservas;
 import pe.edu.upc.vwalletweb.serviceinterfaces.IDetalleReservasService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,5 +42,20 @@ public class DetalleReservasController {
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable("id") Integer id){
         drS.delete(id);
+    }
+
+    @GetMapping("/viajesmasrealizados")
+    public List<totalViajesRealizadosPorTransporteidDTO> Obtenerviajesmasrealizados(){
+        List<String[]> filalistaviajesmasrealizados = drS.totalViajesRealizadosPorTransporteID();
+        List<totalViajesRealizadosPorTransporteidDTO> dtoLista = new ArrayList<>();
+        for(String[] columna:filalistaviajesmasrealizados){
+            totalViajesRealizadosPorTransporteidDTO dto = new totalViajesRealizadosPorTransporteidDTO();
+            dto.setIdTransporte(Integer.parseInt(columna[0]));
+            dto.setUniversidadSalida(columna[1]);
+            dto.setUniversidadLlegada(columna[2]);
+            dto.setTotalNumViajes(Integer.parseInt(columna[3]));
+            dtoLista.add(dto);
+        }
+        return dtoLista;
     }
 }
