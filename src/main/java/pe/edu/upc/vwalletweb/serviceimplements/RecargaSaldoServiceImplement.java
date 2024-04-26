a924957f7bbcc46557e2843e9af58d5577f22bbe
arianjsx <u202116567@upc.edu.pe>
2024-04-26 17:30:30 -0500

    Query 9 fixed
    Parameters to SQL query

--- src/main/java/pe/edu/upc/vwalletweb/serviceimplements/RecargaSaldoServiceImplement.java
+++ src/main/java/pe/edu/upc/vwalletweb/serviceimplements/RecargaSaldoServiceImplement.java
@@ -6,12 +6,14 @@ import pe.edu.upc.vwalletweb.entities.RecargaSaldo;
 import pe.edu.upc.vwalletweb.repositories.IRecargaSaldoRepository;
 import pe.edu.upc.vwalletweb.serviceinterfaces.IRecargaSalgoService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class RecargaSaldoServiceImplement implements IRecargaSalgoService {
     @Autowired
     private IRecargaSaldoRepository rsR;
+
     @Override
     public void insert(RecargaSaldo recargasaldo) {
         rsR.save(recargasaldo);
@@ -29,12 +31,12 @@ public class RecargaSaldoServiceImplement implements IRecargaSalgoService {
 
     @Override
     public List<String[]> TotalRecargadoUsuario() {
-        return rsR.TotalRecargadoUsuario() ;
+        return rsR.TotalRecargadoUsuario();
     }
 
     @Override
-    public List<String[]> FechaRecargaSaldoIntervalo() {
+    public List<String[]> FechaRecargaSaldoIntervalo(LocalDate fInicio, LocalDate fFin) {
 
-        return rsR.FechaRecargaSaldoIntervalo();
+        return rsR.FechaRecargaSaldoIntervalo(fInicio, fFin);
     }
 }
